650e70cf659f32c3155d0f83718be6118ccaaba5
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-04-23 20:35:20 -0300

    refactor: refatorando rota para adicionar middlware de verificar campo

--- src/routes/contatoRoutes.ts
+++ src/routes/contatoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { criar, listar } from "../controllers/contatoController";
+import { verificarCampo } from "../middlewares/verificarCampo";
 
 const routerContato = Router();
 
-routerContato.post("/criar-contato", criar);
+routerContato.post("/criar-contato", verificarCampo, criar);
 routerContato.get("/listar-contatos", listar);
 
 export default routerContato;
