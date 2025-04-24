62bd4c20e60a4cc6becaf5e42999f1c05140ac7c
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-04-23 21:23:23 -0300

    refactor: configurando rota para o midleware de verificar se tem contatos iguais

--- src/routes/contatoRoutes.ts
+++ src/routes/contatoRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { criar, listar } from "../controllers/contatoController";
 import { verificarCampo } from "../middlewares/Contato/verificarCampo";
+import { verificarRepeticao } from "../middlewares/Contato/verificarRepeticao";
 
 const routerContato = Router();
 
-routerContato.post("/criar-contato", verificarCampo, criar);
+routerContato.post("/criar-contato", verificarCampo, verificarRepeticao, criar);
 routerContato.get("/listar-contatos", listar);
 
 export default routerContato;
